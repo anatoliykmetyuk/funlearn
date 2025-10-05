1cbda365f6e2ac0d5e39e1fac350c8ce913745ed
Anatolii Kmetiuk <anatoliykmetyuk@gmail.com>
2025-10-05 12:45:31 +0900

    Scope tech details of the DB layer

--- jvm/src/main/scala/funlearn/db/package.scala
+++ jvm/src/main/scala/funlearn/db/package.scala
@@ -7,11 +7,11 @@ import com.augustnagro.magnum.*
 import funlearn.model.{Deck, CardType}
 
 
-val dataSource: DataSource =
+private val dataSource: DataSource =
   val ds = HikariDataSource()
   ds.setJdbcUrl("jdbc:sqlite:../data/database.db")
   ds
-val transactor = Transactor(dataSource)
+private val transactor = Transactor(dataSource)
 
 val decksRepo = DbAccessLayerRepo(Repo[Deck, Deck, Long])
 val cardTypesRepo = CardTypesRepo(Repo[CardType, CardType, Long])
